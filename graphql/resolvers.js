43096e08ca6c8289c739af15bf910e910f4bf2bb
Coby Lerer <coby_lerer@hotmail.com>
2019-12-14 09:09:10 +1100

    update

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -108,15 +108,40 @@ module.exports = {
             imageUrl: postInput.imageUrl,
             creator: user
         });
+        // create the post in the DB
         const createdPost = await post.save();
-
-        user.posts.push(createPost);
+        // update the user with the new related post
+        user.posts.push(createdPost._id);
+        await user.save();
 
         return {
             ...createdPost._doc, 
             _id:createdPost._id.toString(),
-            createdAt: createPost.createdAt.toISOString(),
-            updatedAt: createPost.updatedAt.toISOString()
+            createdAt: createdPost.createdAt.toISOString(),
+            updatedAt: createdPost.updatedAt.toISOString()
         };
+    },
+    posts: async function({},req){
+        if (!req.isAuth){
+            const error = new Error('Not authenticated');
+            error.statusCode = 401;
+            throw error;
+        }
+
+        const totalPosts = await Post.find().countDocuments();
+        const posts = await Post.find()
+            .sort({createdAt: -1})
+            .populate('creator');
+        return {
+            posts: posts.map(p => {
+                return {
+                    ...p._doc, 
+                    _id:p._id.toString(),
+                    createdAt: p.createdAt.toISOString(),
+                    updatedAt: p.updatedAt.toISOString()
+                }
+            }),
+            totalPosts: totalPosts
+        }
     }
 };
