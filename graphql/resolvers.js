ed6d0c6b5b37e62519bd19a2d7a7775408bd1793
Coby Lerer <coby_lerer@hotmail.com>
2019-12-16 00:28:26 +1100

    adding pagination

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -121,16 +121,21 @@ module.exports = {
             updatedAt: createdPost.updatedAt.toISOString()
         };
     },
-    posts: async function({},req){
+    posts: async function({page},req){
         if (!req.isAuth){
             const error = new Error('Not authenticated');
             error.statusCode = 401;
             throw error;
         }
-
+        if (!page){
+            page = 1;
+        }
+        const perPage = 2;
         const totalPosts = await Post.find().countDocuments();
         const posts = await Post.find()
             .sort({createdAt: -1})
+            .skip((page-1)*perPage)
+            .limit(perPage)
             .populate('creator');
         return {
             posts: posts.map(p => {
